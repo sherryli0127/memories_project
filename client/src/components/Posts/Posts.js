f1580e79d58fa2f2cbfad2244a485728059db832
Sherry Li <sherryli0127@gmail.com>
2021-04-27 16:16:48 -0500

    first commit

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Grid, CircularProgress} from "@material-ui/core";
+import {useSelector} from "react-redux";
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+const Posts = ({setCurrentId}) => {
+  // useSelector to fetch data from the global store
+  //state is the whole store's state
+  //state.posts is defined in reducer index.js
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  console.log(posts);
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default Posts;
